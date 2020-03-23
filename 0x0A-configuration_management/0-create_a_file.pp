c212a5e83fc26117a3d6c6905b92698b0984bf55
Simonster04 <simon.pahe@gmail.com>
2020-03-23 17:05:43 -0500

    correcting puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Using Puppet, create a file in /tmp
 
-file {'holberton':
-  path    => /tmp/holberton,
+file { 'holberton':
+  path    => '/tmp/holberton',
   mode    => 0744,
   owner   => 'www-data',
   group   => 'www-data',
