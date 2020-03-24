cc218c8e112c2a4d0d096905e568af632445f440
Simonster04 <simon.pahe@gmail.com>
2020-03-24 10:42:42 -0500

    using exec

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,8 +1,5 @@
 # make changes to our SSH client configuration file
 
-ssh::sshd_config_match:
-  'User Ubuntu':
-    - 'AllowTcpForwarding yes'
-  'Address 104.196.213.138':
-    - 'PasswordAuthentication no'
-
+exec {'SSH_conf_file':
+  command => 'echo "    PasswordAuthentication no" >> ~/.ssh/config',
+}
