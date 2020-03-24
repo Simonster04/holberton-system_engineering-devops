74d0c484c0ede215c7242ccdcf59cffc4a8d003d
Simonster04 <simon.pahe@gmail.com>
2020-03-24 10:58:23 -0500

    correcting puppet-lint

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,8 +1,6 @@
 # make changes to our SSH client configuration file
 
 file_line {'SSH_conf_file':
-  ensure            => absent,
   path              => '~/.ssh/config',
   line              => '    PasswordAuthentication no',
-  match_for_absence => true,
 }
