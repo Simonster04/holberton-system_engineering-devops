e8c7b6b3ab7d071e15ffccdac0ac0c14f03610e3
Simonster04 <simon.pahe@gmail.com>
2020-03-24 22:23:50 -0500

    correcting path

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # make changes to our SSH client configuration file
 
-file_line {'IdentityFile':
+file_line {'Identity_File':
   ensure => present,
-  path   => '/etc/ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
 }
 
-file_line {'PasswordAuthentication':
+file_line {'Password_Authentication':
   ensure => present,
   path   => '/etc/ssh/config',
   line   => 'PasswordAuthentication no',
