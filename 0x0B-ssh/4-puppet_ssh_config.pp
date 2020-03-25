91c634ae653115c5beb2b4df8f7b1e6cf75b28ba
Simonster04 <simon.pahe@gmail.com>
2020-03-24 22:14:27 -0500

    adding privaty key condition

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,12 @@
 # make changes to our SSH client configuration file
 
-file_line {'SSH_conf_file':
+file_line {'IdentityFile':
+  ensure => present,
+  path   => '~/.ssh/config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line {'PasswordAuthentication':
   ensure => present,
   path   => '~/.ssh/config',
   line   => 'PasswordAuthentication no',
