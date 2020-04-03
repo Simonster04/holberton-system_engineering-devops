078baf6437709f6e208986730bae89de86d03358
Simonster04 <simon.pahe@gmail.com>
2020-04-03 17:03:45 -0500

    removing sudo puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,6 +3,6 @@
 #   Value of the header is the hostname of the server Nginx running on
 
 exec {'http_header':
-  command  => "sudo apt-get -y update && sudo apt-get -y install nginx && echo 'Holberton School' | sudo tee /var/www/html/index.nginx-debian.html && sudo sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default && sudo sed -i '19i $\\\tadd_header X-Served-By $HOSTNAME;' /etc/nginx/sites-available/default && sudo service nginx restart && sudo service nginx reload",
+  command  => "apt-get -y update && apt-get -y install nginx && echo 'Holberton School' | sudo tee /var/www/html/index.nginx-debian.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default && sed -i '19i $\\\tadd_header X-Served-By $HOSTNAME;' /etc/nginx/sites-available/default && service nginx restart && service nginx reload",
   provider => 'shell',
 }
