b5e4d8f89479cb7f174ef4231b7b1dd45d4094b6
Simonster04 <simon.pahe@gmail.com>
2020-04-03 17:21:22 -0500

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,6 +3,6 @@
 #   Value of the header is the hostname of the server Nginx running on
 
 exec {'http_header':
-  command  => "sudo apt-get -y update && sudo apt-get -y install nginx && sudo service nginx start && sudo sed -i "19i \\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default && sudo service nginx restart && sudo service nginx reload",
+  command  => "sudo apt-get -y update && sudo apt-get -y install nginx && sudo service nginx start && sudo sed -i '19i \\\tadd_header X-Served-By $hostname;' /etc/nginx/sites-available/default && sudo service nginx restart && sudo service nginx reload",
   provider => 'shell',
 }
