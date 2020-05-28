92d421d5ff97007008d11e1dd7e026b1e5353caa
Simonster04 <55111089+Simonster04@users.noreply.github.com>
2020-05-27 22:53:14 -0500

    Delete 0-the_sky_is_the_limit_not.pp

--- 0-the_sky_is_the_limit_not.pp/0-the_sky_is_the_limit_not.pp
+++
@@ -1,7 +0,0 @@
-#Fixing failed requests when ab
-#by adding more worker processes
-
-exec {'adding_more_worker_processes':
-  path    => ['/usr/bin', '/usr/sbin', '/bin'],
-  command => "sed -i 's/worker_processes 4/worker_processes 40/g' /etc/nginx/nginx.conf && service nginx restart",
-}
